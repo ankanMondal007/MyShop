cfaa9e0b0f6da377d735a7af6a60c41490f4737c
Ankan <mailtoankan@gmail.com>
2023-01-12 21:55:23 +0530

    Pushed to Github

---
+++ frontend/build/precache-manifest.a72b0c41b742da50c1886aa50feee52f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8ed5fc6f95463f03d72862cb3456ac7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "09b1b7e27dbbc3785ec6",
+    "url": "/static/css/main.98bb7763.chunk.css"
+  },
+  {
+    "revision": "313ccda13af906be5d2b",
+    "url": "/static/js/2.493e2401.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.493e2401.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09b1b7e27dbbc3785ec6",
+    "url": "/static/js/main.13e5a35a.chunk.js"
+  },
+  {
+    "revision": "0b6b46ef1e17d32e6df4",
+    "url": "/static/js/runtime-main.bce919bb.js"
+  }
+]);
